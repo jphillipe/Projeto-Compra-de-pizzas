6cf1311dd7d237860fcb2acd40a374eab5d24d6d
João Phillipe Fernandes <joao.phillipe@ufba.br>
2024-01-08 03:03:10 -0300

    initial config

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//teste//
-////
-document.querySelector('.teste');
+const q = (el)=> document.querySelector(el);
+const qa = (el)=> document.querySelectorAll(el);
 
-// teste 4 ///
+pizzaJson.map((item, index)=>{
+    let pizzaItem = q('.models .pizza-item').cloneNode(true);
+    q('.pizza-area').append(pizzaItem);
+})
