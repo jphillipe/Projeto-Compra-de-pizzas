7d10ef9bf874a867df7e02d967253de68d9e2ec8
João Phillipe Fernandes <joao.phillipe@ufba.br>
2024-01-12 17:35:18 -0300

    Preenchimento de informações no modal

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const qa = (el)=> document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
     let pizzaItem = q('.models .pizza-item').cloneNode(true);
 
+
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click',(e) =>{
         e.preventDefault();
+
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        q('.pizzaBig img').src = pizzaJson[key].img;
+        q('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        q('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
 
         q('.pizzaWindowArea').style.opacity = 0;
         q('.pizzaWindowArea').style.display = 'flex';
